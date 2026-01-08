bb3b27b9c34346573bf823f7c63d2763d1e7355d
Guru Prasad Shukla <shuklaguru3@gmail.com>
2026-01-08 10:56:07 +0530

    init commit

---
+++ src/main/java/org/example/VahnApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class VahnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VahnApplication.class, args);
+    }
+}
